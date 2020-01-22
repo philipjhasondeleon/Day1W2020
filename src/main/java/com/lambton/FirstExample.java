1e7250d125646ef3bd31e5d2bfd127372e97997a
jhasondeleon <jhasondeleon@gmail.com>
2020-01-22 18:49:24 -0500

    Day 2 - Data types, variable declaration and type conversion

--- src/main/java/com/lambton/FirstExample.java
+++ src/main/java/com/lambton/FirstExample.java
@@ -9,7 +9,42 @@ public class FirstExample
 
             System.out.println("PhilipJhasonDeLeon");
 
-        //sample commit - 1
+        //Day - 2 Code Starts here
 
+        int a = 100;
+        int b;
+
+        b = 200;
+
+        int c = a + b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        System.out.println("Sum = " + a + b);
+        System.out.println(a + b +  "Sum");
+
+        short x = 32767;
+        System.out.println("Short MAX = " + Short.MAX_VALUE);
+        System.out.println("Short MIN = " + Short.MIN_VALUE);
+
+        long y = 2147483647;
+        System.out.println(y);
+
+
+        float r1 = 1000.50F;
+        double r2 =1000.40D;
+        float r3 = (float)10/3;
+        System.out.println("R1 = " + r1);
+        System.out.println("R2 = " + r2);
+        System.out.println("R3 = " + r3);
+
+        int z = 10 / 3;
+        float z1 = 10.0f / 3;
+        System.out.println("z = " + z);
+        System.out.println("z = " + z1);
+
+        double x1 = 1_00___00.5_0;
     }
 }
