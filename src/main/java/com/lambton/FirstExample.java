6b3092211cdac37a05622f767c1b4bad894a7d27
MacStudent <macstudent@128-iMAC30.local>
2020-01-21 20:50:20 -0500

    [Philip]Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,13 @@
+package com.lambton;
+
+public class FirstExample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java Programming");
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("PhilipJhasonDeLeon");
+        }
+    }
+}
